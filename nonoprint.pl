2a53ace3c02af0d842ff6cd9e5f0c3745c8eeaca
Kaoru Maeda <kaoru.maeda@gmail.com>
2011-01-09 00:54:46 +0900

    answer

---
+++ nonoprint.pl
@@ -0,0 +1,32 @@
+use 5.010;
+use strict;
+use warnings;
+
+sub main {
+  while (<>) {
+    next if /#/;
+    my @pats = split(/ /, $_);
+    my @board = ("______") x 6;
+    for my $i (0..3) {
+      fill(\@board, $pats[$i], $i+1);
+    }
+    print join("\n", @board), "\n\n";
+  }
+}
+
+sub fill {
+  my ($board, $pat, $char) = @_;
+  my @hx = $pat =~ /(..)/g;
+  shift @hx; pop @hx;
+  @hx = map { unpack('B8', pack('H2', $_)) } @hx;
+  for my $y (0..5) {
+    for my $x (0..5) {
+      if (substr($hx[$y], $x+1, 1) eq '1') {
+	substr($board->[$y], $x, 1) = $char;
+      }
+    }
+  }
+}
+
+main unless caller();
+1;
